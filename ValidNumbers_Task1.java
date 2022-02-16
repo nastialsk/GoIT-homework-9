e73116ac4c600f7704d31dab2b36186a99d914de
nastialsk <95052689+nastialsk@users.noreply.github.com>
2022-02-16 16:30:08 +0200

    Update ValidNumbers_Task1.java

--- ValidNumbers_Task1.java
+++ ValidNumbers_Task1.java
@@ -1,19 +1,20 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class ValidNumbers {
-    private static final String line = "file.txt";
+   private static final File file = new File("C:\\file.txt");
 
-    static String stringCheckResult(String line) {
-        Matcher patternMatcher = Pattern.compile("(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}").matcher(line);
-        return patternMatcher.matches() ? "OK" : "ERROR";
+    static String stringCheckResult(String file) {
+        Matcher patternMatcher = Pattern.compile("(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}").matcher(file);
+        return String.valueOf(patternMatcher.matches());
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader inputFileReader = new BufferedReader(new FileReader(line));
+        BufferedReader inputFileReader = new BufferedReader(new FileReader(file));
         String currentFileString;
         while ((currentFileString = inputFileReader.readLine()) != null) {
             System.out.println(currentFileString + " - " + stringCheckResult(currentFileString));
